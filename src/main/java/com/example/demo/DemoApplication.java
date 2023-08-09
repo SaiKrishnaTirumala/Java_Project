3f6be36abc684a133bcd1118b618aa0aba38841e
SaiKrishnaTirumala <125542725+SaiKrishnaTirumala@users.noreply.github.com>
2023-08-09 18:37:25 +0530

    Update DemoApplication.java

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -4,9 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-
+   @GetMapping("/")
 public class DemoApplication {
-
+     public String home(){
+	     return "Hi";
+     }
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
